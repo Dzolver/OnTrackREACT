Dzolver/OnTrackREACT

commit 73445c12fcf6334208bb6b57961388e3da696c26

Added Login Button but logic returns Fail Toast

loginform.js
@@ -2,10 +2,25 @@ import React, {Component} from 'react';
 import {
   StyleSheet,
   View,
-  TextInput
+  TextInput,
+  Button,
+  ToastAndroid
 } from 'react-native';
 
 export default class LoginForm extends Component {
+  state = {
+    username:'',
+    password:'',
+  }
+  //checkDetails password or username check DOESNT WORK!! FIX
+  _checkDetails() {
+    if(String(this.password) === '123'){
+      ToastAndroid.show('Success!', ToastAndroid.SHORT);
+    } else {
+      ToastAndroid.show('Failed!', ToastAndroid.SHORT);
+    }
+  }
+  //////////////////////////////////////////////////////////////
   render(){
     return(
       <View style={styles.container}>
@@ -13,12 +28,15 @@ export default class LoginForm extends Component {
         placeholder="Username or Email"
         placeholderTextColor="rgba(255,255,255,0.7)"
         underlineColorAndroid = "transparent"
+        onChangeText={(username) => this.setState({username})}
         style={styles.input}/>
         <TextInput
         placeholder="Password"
         placeholderTextColor="rgba(255,255,255,0.7)"
         underlineColorAndroid = "transparent"
+        onChangeText={(password) => this.setState({password})}
         style={styles.input}/>
+        <Button onPress={this._checkDetails} title='login'/>
       </View>
     );
   }
@@ -30,7 +48,7 @@ const styles = StyleSheet.create({
   },
   input: {
     height : 40,
-    backgroundColor: 'rgba(255,255,255,0.4)',
+    backgroundColor: 'rgba(255,255,255,0.7)',
     marginBottom:20,
     color : '#FFF',
     paddingHorizontal: 10

commit a2bfdab06de504c88c4c5bcc84543fae80cd56a5

Fix issue introduced in 73445c12fcf6334208bb6b57961388e3da696c26
This commit fixes the problems with the login form not correctly parsing input. The error was introduced due to incorrect binding and referencing of the component's state variable.

loginform.js
@@ -8,18 +8,25 @@ import {
 } from 'react-native';
 
 export default class LoginForm extends Component {
-  state = {
-    username: '',
-    password: '',
+
+  //constructor method, to properly declare all needed variables
+  constructor(props) {
+    super(props);                                         // grab props from app component, if needed
+    this._checkDetails = this._checkDetails.bind(this);   // bind method 'this' to component 'this', to reference 'this.state' properly
+    this.state = {                                        // declare component state (note use of 'this' keyword)
+      username: '',
+      password: ''
+    };
   }
-  //checkDetails password or username check DOESNT WORK!! FIX
+
   _checkDetails() {
-    if (String(this.password) === '123') {
+    if (String(this.state.password) === '123') {          //reference state variable (note use of 'this.state.password', as opposed to 'this.state')
       ToastAndroid.show('Success!', ToastAndroid.SHORT);
     } else {
       ToastAndroid.show('Failed!', ToastAndroid.SHORT);
     }
   }
+
   //////////////////////////////////////////////////////////////
   render() {
     return (